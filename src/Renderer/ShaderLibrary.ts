e2987a5fbe04dece00698e337e45ae01bd9c39e0
JellyAlex <alexbr5@bigpond.com>
2019-09-24 21:05:36 +0800

    Not working

---
+++ src/Renderer/ShaderLibrary.ts
@@ -0,0 +1,29 @@
+import Shader from './Shader';
+
+/** Stores a number of shaders. */
+export default class ShaderLibrary {
+    private shaders: { [id: string]: Shader };
+
+    /**
+     * Add a shader to be stored in the shader library.
+     * @param {Shader} shader The shader to add.
+     * @param {string} [name] The name used to access the shader (optional).
+     */
+    public add(shader: Shader, name: string = null): void {
+        if (!name) {
+            name = shader.getName();
+        }
+        console.assert(!(name in this.shaders), 'Shader already exists!');
+
+        this.shaders[name] = shader;
+    }
+
+    public async load(filepath: string, name: string = null): Promise<Shader> {
+
+        return Shader.loadFromFetch(filepath, name).then((shader: Shader) => {
+            this.add(shader, name);
+        });
+    }
+
+
+}
